c2a86782bde34ee735b47ca27abbd291193a5186
Martin Kelly <marty.kelly2009@gmail.com>
2017-05-10 10:43:39 +0100

    added sample code for long tap

--- Map Testing/ViewController.swift
+++ Map Testing/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
+    var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.mapView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
+}
 
-
+extension ViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didLongPressAt coordinate: CLLocationCoordinate2D) {
+        // Custom logic here
+        let marker = GMSMarker()
+        marker.position = coordinate
+        marker.title = "I added this with a long tap"
+        marker.snippet = ""
+        marker.map = mapView
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,5 @@
+# Uncomment this line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+target 'Map Testing' do
+  pod 'GoogleMaps'
+end
